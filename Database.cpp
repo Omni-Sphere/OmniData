cc2b2319a1bdf3b2c57418be713857160786a0a0
= <=>
2026-06-08 19:07:19 -0600

    refactor: improve code style and formatting consistency across database classes

--- Database.cpp
+++ Database.cpp
@@ -2,14 +2,18 @@
 #include <SQLServerDatabase.hpp>
 #include <MySQLDatabase.hpp>
 
-namespace omnisphere::services {
-
-Database::Database(DatabaseEngine engine) {
-  if (engine == DatabaseEngine::MySQL) {
-    _impl = std::make_unique<MySQLDatabase>();
-  } else {
-    _impl = std::make_unique<SQLServerDatabase>();
-  }
-}
+namespace omnisphere::services
+{
+    Database::Database(DatabaseEngine engine)
+    {
+        if (engine == DatabaseEngine::MySQL)
+        {
+            _impl = std::make_unique<MySQLDatabase>();
+        }
+        else
+        {
+            _impl = std::make_unique<SQLServerDatabase>();
+        }
+    }
 
 } // namespace omnisphere::services
